c511d930b489475ad4d5248a0e385e3d8be31eff
Ayana Bando <ayanaflax@yahoo.com>
2018-04-10 14:53:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
     elsif array.length == 2
       array.join(" and ")
     else array.length >= 3
-      array.join(" and ")
+      "#{array[0..-2].join(" , ") + " , and " + array[-1]}"
   end
 end
